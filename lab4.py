af61c23f54651fa7afd9cebf001976c9eb19c8e0
Mauricio <masagi94@gmail.com>
2017-10-09 22:47:03 -0400

    Part 1 complete

--- lab4.py
+++ lab4.py
@@ -1,148 +1,126 @@
-import time
-import RPi.GPIO as  GPIO
-
-
-
-# Should we keep this function? Idk how much we're allowed to use from the provided code..
-# def __init__(self,in1=12,in2=13,ena=6,in3=20,in4=21,enb=26):
-# 		self.IN1 = in1
-# 		self.IN2 = in2
-# 		self.IN3 = in3
-# 		self.IN4 = in4
-# 		self.ENA = ena
-# 		self.ENB = enb
-
-		# Sets the the pin numbering mode to BCM
-GPIO.setmode(GPIO.BCM)
-
-# If the Pi has more than one script/circuit on the GPIO, warnings will print out.
-# This turns off those warnings.
-GPIO.setwarnings(False)
-
-GPIO.setup(12,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(20,GPIO.OUT)
-GPIO.setup(21,GPIO.OUT)
-GPIO.setup(6,GPIO.OUT)
-GPIO.setup(26,GPIO.OUT)
-
-#self.forward()
-# ena
-GPIO.PWM(6,500)
-GPIO.PWM(6,500).start(50)
-
-# enb
-GPIO.PWM(26,500)
-GPIO.PWM(26,500).start(50)
-
-# self.PWMA = GPIO.PWM(self.ENA,500)
-# self.PWMB = GPIO.PWM(self.ENB,500)
-# self.PWMA.start(50)
-# self.PWMB.start(50)
-
-# Functions to carry out movement of the robot
-# Can make funtion move robot by a set amount each time, or use parameters for desired amount
-# 0 = GPIO.LOW	1 = GPIO.HIGH
-def moveForward():
-        print("Moving Forward...")
-        GPIO.output(12,1)
-        GPIO.output(13,0)
-        GPIO.output(20,0)
-        GPIO.output(21,1)
-
-def stop():
-        print("Stopping...")
-        GPIO.output(12,0)
-        GPIO.output(13,0)
-        GPIO.output(20,0)
-        GPIO.output(21,0)
-
-def moveBackward():
-        print("Moving Backward...")
-        GPIO.output(12,0)
-        GPIO.output(13,1)
-        GPIO.output(20,1)
-        GPIO.output(21,0)
-
-def moveLeft():
-        print("Moving Left...")
-        GPIO.output(12,0)
-        GPIO.output(13,0)
-        GPIO.output(20,0)
-        GPIO.output(21,1)
-
-def moveRight():
-        print("Moving Right...")
-        GPIO.output(12,1)
-        GPIO.output(13,0)
-        GPIO.output(20,0)
-        GPIO.output(21,0)
-
-def pivotRight():
-		print("Right Pivot...")
-        GPIO.output(12,1)
-        GPIO.output(13,0)
-        GPIO.output(20,1)
-        GPIO.output(21,0)
-
-def pivotLeft():
-		print("Left Pivot...")
-        GPIO.output(12,0)
-        GPIO.output(13,1)
-        GPIO.output(20,0)
-        GPIO.output(21,1)
-
-
-# Forward and backward movement
-moveForward()
-time.sleep(5)
-stop()
-time.sleep(1)
-moveBackward()
-time.sleep(10)
-stop()
-time.sleep(1)
-moveForward()
-time.sleep(5)
-stop()
-time.sleep(1)
-
-# Movement to the right and back to the center
-# He wants a pivot, not a turn left or turn right. Made a function for that, dont know if it works
-pivotRight()
-time.sleep(1) # How much time would be needed to pivot 90%?
-stop()
-time.sleep(1)
-moveForward()
-time.sleep(5)
-stop()
-time.sleep(1)
-moveBackward()
-time.sleep(5)
-stop()
-time.sleep(1)
-
-
-# Movement to the left and back to the center
-pivotLeft()
-time.sleep(2) # Again, how much time would pivoting take?
-stop()
-moveForward()
-time.sleep(5)
-stop()
-time.sleep(1)
-moveBackward()
-time.sleep(5)
-stop()
-time.sleep(1)
-
-# Face forward, ends movement.
-pivotRight()
-time.sleep(1)
-stop()
-
-
-
-
-# Cleans up the used resources
+import time                                                                              
+import RPi.GPIO as  GPIO                                                                 
+                                                                                         
+                                                                                         
+                                                                                         
+#Functions to carry out movement of the robot                                            
+# 0 = GPIO.LOW  1 = GPIO.HIGH                                                            
+def moveForward():                                                                       
+        print("Moving Forward...")                                                       
+        GPIO.output(12,1)                                                                
+        GPIO.output(13,0)                                                                
+        GPIO.output(20,0)                                                                
+        GPIO.output(21,1)                                                                
+                                                                                         
+def stop():                                                                              
+        print("Stopping...")                                                             
+        GPIO.output(12,0)                                                                
+        GPIO.output(13,0)                                                                
+        GPIO.output(20,0)                                                                
+        GPIO.output(21,0)                                                                
+                                                                                         
+def moveBackward():                                                                      
+        print("Moving Backward...")                                                      
+        GPIO.output(12,0)                                                                
+        GPIO.output(13,1)                                                                
+        GPIO.output(20,1)                                                                
+        GPIO.output(21,0)                                                                
+                                                                                         
+def pivotRight():                                                                        
+        print("Right Pivot...")                                                          
+        GPIO.output(12,1)                                                                
+        GPIO.output(13,0)                                                                
+        GPIO.output(20,1)                                                                
+        GPIO.output(21,0)                                                                
+                                                                                         
+def pivotLeft():                                                                         
+        print("Left Pivot...")                                                           
+        GPIO.output(12,0)                                                                
+        GPIO.output(13,1)                                                                
+        GPIO.output(20,0)                                                                
+        GPIO.output(21,1)                                                                
+                                                                                         
+                                                                                         
+# Sets the desired pin numbering system to BCM                                           
+GPIO.setmode(GPIO.BCM)                                                                   
+                                                                                         
+# Disables warnings in case the RPI.GPIO detects that a pin has been configured          
+# to something other than the default (input)                                            
+GPIO.setwarnings(False)                                                                  
+                                                                                         
+# These are the pins we will be using.                                                   
+# 6 and 26 are ena and enb                                                               
+# 12, 13, 20, 21 are IN1 IN2 IN3 IN4                                                     
+chan_list = [6,12,13,20,21,26]                                                           
+                                                                                         
+# Sets all the pins stated above as outputs                                              
+GPIO.setup(chan_list,GPIO.OUT)                                                           
+                                                                                         
+# creates objects "p1" and "p2", sets ena and enb to 50 Hz, starts them at 20% duty cycle
+p1 = GPIO.PWM(6,50)                                                                      
+p1.start(20)                                                                             
+                                                                                         
+p2 = GPIO.PWM(26,50)                                                                     
+p2.start(20)
+                                                                               
+# Forward and backward movement                                                
+# Used the sleep() function from the time library to create a delay in seconds 
+moveForward()                                                                  
+time.sleep(5)                                                                  
+stop()                                                                         
+time.sleep(1)                                                                  
+moveBackward()                                                                 
+time.sleep(10)                                                                 
+stop()                                                                         
+time.sleep(1)                                                                  
+moveForward()                                                                  
+time.sleep(5)                                                                  
+stop()                                                                         
+time.sleep(1)                                                                  
+                                                                               
+# Pivots to the right by 90%.                                                  
+# Movement to the right and back to the center                                 
+pivotRight()                                                                   
+time.sleep(.7) # .7 seconds = about 90%                                        
+stop()                                                                         
+time.sleep(1)                                                                  
+moveForward()                                                                  
+time.sleep(5)                                                                  
+stop()                                                                         
+time.sleep(1)                                                                  
+moveBackward()                                                                 
+time.sleep(5)                                                                  
+stop()                                                                         
+time.sleep(1)                                                                  
+                                                                               
+                                                                               
+# Pivots to the left by 180%                                                   
+# Movement to the left and back to the center                                  
+pivotLeft()                                
+time.sleep(1.25) # 1.25 second = about 180%
+stop()                                     
+time.sleep(1)                              
+                                           
+moveForward()                              
+time.sleep(5)                              
+stop()                                     
+time.sleep(1)                              
+moveBackward()                             
+time.sleep(5)                              
+stop()                                     
+time.sleep(1)                              
+                                           
+# Pivots to the right by 90%               
+# Ends movement                            
+pivotRight()                               
+time.sleep(.7) # .7 seconds = about 90%    
+stop()                                     
+                                           
+# Stops both the PWM outputs               
+p1.stop()                                  
+p2.stop()                                  
+                                           
+                                           
+                                           
+# Cleans up the used resources             
 GPIO.cleanup()
